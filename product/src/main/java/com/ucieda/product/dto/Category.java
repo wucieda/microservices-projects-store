ef84bd479f7076f976a62f7eac9e1fbf1341462a
wucieda <wilder.ucieda@dharbor.com>
2021-03-02 14:59:08 -0400

    Let's speed up the coding with Lombok & Codota

--- product/src/main/java/com/ucieda/product/dto/Category.java
+++ product/src/main/java/com/ucieda/product/dto/Category.java
@@ -2,7 +2,6 @@ package com.ucieda.product.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Getter
 @Setter
@@ -10,29 +9,4 @@ public class Category {
     private Integer id;
     private String name;
     private String brand;
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
 }
